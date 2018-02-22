3d24f5379583f3ed250fe584a5db4b2f88d98b15
Andrew Mackowski <andrew@veryapt.com>
2018-02-22 18:35:04 -0500

    hmm

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='django-phone-field',
-    version='1.1.0',
+    version='1.2.0',
     url=r'https://github.com/VeryApt/django-phone-field/',
     license='GPL',
     platforms=['OS Independent'],
